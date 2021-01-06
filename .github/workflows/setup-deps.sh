7e595069a44151c2c8beb22346319340505f179b
h1994st <h1994st@gmail.com>
2021-01-06 16:17:16 -0500

    Update setup-deps.sh

--- .github/workflows/setup-deps.sh
+++ .github/workflows/setup-deps.sh
@@ -3,5 +3,4 @@
 set -e
 set -x
 
-wget https://github.com/tamarin-prover/tamarin-prover/releases/download/1.6.0/tamarin-prover-1.6.0-linux64-ubuntu.tar.gz
-tar -xzf tamarin-prover-1.6.0-linux64-ubuntu.tar.gz
+brew install tamarin-prover/tap/tamarin-prover
