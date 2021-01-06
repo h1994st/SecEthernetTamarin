bf6a904c65c79f4fedab586acba6e755cff0c184
h1994st <h1994st@gmail.com>
2021-01-06 16:03:32 -0500

    Add CI

---
+++ .github/workflows/setup-deps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+set -e
+set -x
+
+wget https://github.com/tamarin-prover/tamarin-prover/releases/download/1.6.0/tamarin-prover-1.6.0-linux64-ubuntu.tar.gz
+tar -xzf tamarin-prover-1.6.0-linux64-ubuntu.tar.gz
